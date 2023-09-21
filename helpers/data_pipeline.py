9d00b6f0798e6875fea4db284e8c39c827984d0a
nikitawilms <nikdotde@gmail.com>
2023-09-21 10:47:18 +0200

    updated pipeline

--- helpers/data_pipeline.py
+++ helpers/data_pipeline.py
@@ -1,20 +1,16 @@
 # Data Pipeline
 def data_pipeline(df):
+    # add new data in csv format
+
     # Stage 1: Text Preprocessing
     df["preprocessed_text"] = df["text"].apply(preprocess_text)
 
     # Stage 2: Adding spelling correction
     df["preprocessed_text"] = df["preprocessed_text"].apply(add_spelling_correction)
-    
+
     # Stage 3: Adding ngrams
     df["preprocessed_text"] = df["preprocessed_text"].apply(add_ngrams)
 
-    # Stage 3: Removing most common words
-    # df["preprocessed_text"] = df["preprocessed_text"].apply(remove_most_common_words)
-
-    # Stage 4: Removing least common words
-    # df["preprocessed_text"] = df["preprocessed_text"].apply(remove_least_common_words)
-
-    # Stage 5: Add to csv, if csv is not empty
+    # Stage 4: Add to csv, if csv is not empty
 
     return df
