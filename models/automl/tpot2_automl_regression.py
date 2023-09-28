a0b54cc3e2a3c0a7ab576b524c6864a9cd6b323b
nikitawilms <nikdotde@gmail.com>
2023-09-28 11:16:32 +0200

    first trial run autoML

--- models/automl/tpot2_automl_regression.py
+++ models/automl/tpot2_automl_regression.py
@@ -1,57 +1,56 @@
 from sklearn.model_selection import train_test_split, KFold
-from sklearn.metrics import mean_squared_error, make_scorer
+
+# from sklearn.metrics import mean_squared_error, make_scorer
 import tpot2
+import sklearn
+import numpy as np
 import pandas as pd
 
-# Read your data
-# Assuming 'df' is your DataFrame and 'ESG_Score' is your target column
-df = pd.read_csv("path/to/your/esg_data.csv")
+if __name__ == "__main__":
+    df = pd.read_csv("data/model_data/model_data.csv")
+    df_sample = df.sample(100, random_state=100)
 
-# Separate features and target
-X = df.drop(["ESG_Score"], axis=1)
-y = df["ESG_Score"]
+    # Separate features and target
+    X = df.drop(["ESG_Score"], axis=1)
+    y = df["ESG_Score"]
 
-# Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
+    # Split the data into training and test sets
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=100
+    )
 
-# Create a custom scorer (optional)
-scorer = make_scorer(mean_squared_error, greater_is_better=False)
+    scorer = sklearn.metrics.get_scorer("neg_mean_squared_error")
 
-# Initialize TPOT2 regressor with K-Fold cross-validation
-# Make sure to adjust the generations and population_size based on your computational resources and needs. 
-est = tpot2.TPOTEstimator(
-    generations=5,
-    population_size=20,
-    n_jobs=-1,
-    cv=KFold(n_splits=5),  # 5-Fold cross-validation
-    verbosity=2,
-    classification=False,
-    scorers=[scorer],
-    scorers_weights=[1],
-)
+    # Initialize TPOT2 regressor with K-Fold cross-validation
+    est = tpot2.TPOTEstimatorSteadyState(
+        n_jobs=7,
+        cv=KFold(n_splits=5),  # 5-Fold cross-validation
+        verbose=2,
+        classification=False,
+        scorers=[scorer],
+        scorers_weights=[1],
+        max_eval_time_seconds=60 * 15,
+        max_time_seconds=60 * 90,
+    )
 
-# Fit the model
-est.fit(X_train, y_train)
+    # Fit the model
+    est.fit(X_train, y_train)
 
-# Evaluate on test data
-test_score = est.score(X_test, y_test)
+    df_individuals = est.evaluated_individuals
 
-# Print test score
-print(f"Test Score (Negative MSE): {test_score}")
+    # Convert the 'mean_squared_error' column to numeric, errors='coerce' will replace non-numeric with NaN
+    df_individuals["mean_squared_error"] = pd.to_numeric(
+        df_individuals["mean_squared_error"], errors="coerce"
+    )
 
-# Get the best pipeline
-print(f"Optimized Pipeline: {est.fitted_pipeline_}")
+    # Drop NaN values
+    filtered_df = df_individuals.dropna(subset=["mean_squared_error"])
 
-# Get top models in the Pareto front
-pareto_front = est.evaluated_individuals[est.evaluated_individuals['Pareto_Front'] == 1]
-print(f"Top models in the Pareto front: {pareto_front}")
+    # Sort the DataFrame by 'mean_squared_error' and get the top 10
+    top_10_mse = filtered_df.nlargest(10, "mean_squared_error")
 
-# Optionally, you can save the pipeline to a file
-# with open("optimized_pipeline_regression.py", "w") as f:
-#     f.write(est.export())
+    print(top_10_mse)
 
-# Optionally, you can save the pipeline to a file
-# with open("optimized_pipeline_regression.py", "w") as f:
-#     f.write(est.export())
+    # Optionally, you can save the pipeline to a file
+    # with open("optimized_pipeline_regression.py", "w") as f:
+    #     f.write(est.export())
