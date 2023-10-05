b23951978f9f7bdad5177c5ee6c9b960a78f6156
mariusbosch <bosch.marius@icloud.com>
2023-10-05 13:44:43 +0200

    LSTM Marius

---
+++ models/LSTM/LSTM_tune.py
@@ -0,0 +1,26 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dropout, Dense
+from kerastuner.tuners import RandomSearch
+
+def build_model(hp, embedding_dim):
+    model = Sequential()
+    model.add(LSTM(units=hp.Int('units', min_value=32, max_value=512, step=32),
+                   input_shape=(1, embedding_dim), return_sequences=True))
+    model.add(Dropout(rate=hp.Float('dropout_1', min_value=0.1, max_value=0.5, step=0.1)))
+    model.add(LSTM(units=hp.Int('units', min_value=32, max_value=512, step=32)))
+    model.add(Dropout(rate=hp.Float('dropout_2', min_value=0.1, max_value=0.5, step=0.1)))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    return model
+
+def run_tuner(X_train, y_train, X_val, y_val, embedding_dim):
+    tuner = RandomSearch(
+        lambda hp: build_model(hp, embedding_dim),
+        objective='val_loss',
+        max_trials=5,
+        executions_per_trial=3,
+        directory='my_dir',
+        project_name='helloworld'
+    )
+
+    tuner.search(X_train, y_train, epochs=50, validation_data=(X_val, y_val))
